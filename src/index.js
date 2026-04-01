5c93a07791e35fb9d0400d3210e9da76705c5541
harshsankhla18 <harsh18sankhlamy@gmail.com>
2026-04-01 21:00:29 +0530

    feat: add util code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
 dotenv.config({
     path: './.env'
 });
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+            console.log("Err:",err);
+            throw err
+        })
+    app.listen(process.env.PORT,()=>{
+            console.log("App sun rha hai....");
+        })
+    }
+)
