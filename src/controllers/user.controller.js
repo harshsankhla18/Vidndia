9a8058077e3a485fdbb7f6970c90bc773098dcab
harshsankhla18 <harsh18sankhlamy@gmail.com>
2026-07-03 14:58:54 +0530

    Fix: Add validation for refresh token to ensure user existence before proceeding

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -126,7 +126,9 @@ const refreshAccessToken = asyncHandler(async (req,res) => {
    try {
     const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET);
     const user = await User.findById(decodedToken._id);
-    
+    if (!user) {
+    throw new ApiError(401, "Invalid Refresh Token");
+    }
     if(incomingRefreshToken !== user?.refreshToken){
      throw new ApiError(400,"Refresh Token Expired")
     }
